3a6b893b14c58c2d9be4f600164966cc0067dc21
Pascal <pbienenstein@posteo.de>
2024-09-28 01:29:23 +0200

    delete things

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   console.log("Fick dich welt");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>labpa</h1>
+        <h2>Vom Hauptschüler zum Softwareentwickler</h2>
+      </div>
   );
 }
 
