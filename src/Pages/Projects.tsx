505547cee14c7414f4824fad5e184963b72a4bf5
Pascal <pbienenstein@posteo.de>
2024-10-08 18:45:03 +0200

    wip -> Projects

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -1,7 +1,9 @@
 import React, {FC} from "react";
-import {Badge, Button, Card, Carousel, Col, Container, Row} from "react-bootstrap";
+import {Alert, Button, Card, Carousel, Container} from "react-bootstrap";
 import ParticlesComponent from "../Components/particles";
 import Image from "react-bootstrap/Image";
+import {Link} from "react-router-dom";
+
 
 // Bilder Fahrtenbuch
 import account from "../Images/Fahrtenbuch/account.jpg";
@@ -17,65 +19,64 @@ import warnung from "../Images/Abfrageterminal/abfrage-warnung.jpg"
 
 const Projects : FC = () => {
     return(
-        <Container className="container-sm d-flex flex-column justify-content-center align-items-center text-center p-3">
-            <ParticlesComponent id="particles" />
+        <Container
+            className="container-sm d-flex flex-column justify-content-center align-items-center text-center p-3">
+            <ParticlesComponent id="particles"/>
             <h1 className="mb-3 text-white">Projekte</h1>
 
-                    <Card className="text-center" style={{backgroundColor: "transparent", color: "white", maxWidth:"600px"}}>
-                        <Card.Body>
-                            <Card.Title>Abfrage Terminal</Card.Title>
-                            <Carousel>
-                                <Carousel.Item>
-                                    <Image src={startbildschirm} alt="Start Screen" fluid />
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <Image src={positiv} alt="Positive Outcome" fluid />
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <Image src={warnung} alt="Warning Message" fluid />
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <Image src={negativ} alt="Negative Outcome" fluid />
-                                </Carousel.Item>
-                            </Carousel>
-                            <Card.Text>
-                                Implementierung einer Software für ein Terminal zur Abfrage von Arbeitszeiten und Informationen zur anstehenden Schicht mit Hilfe von NFC-Technologie
-                            </Card.Text>
-                            <Button variant="primary" onClick={() => console.log("Button clicked!")}>
-                                Go somewhere
-                            </Button>
-                        </Card.Body>
-                    </Card>
+            <Card className="text-center" style={{backgroundColor: "transparent", color: "white", maxWidth: "600px"}}>
+                <Card.Body>
+                    <Alert style={{backgroundColor: "black", color: "white", border: "1px solid white"}}>
+                        <Alert.Heading>Abfrage Terminal</Alert.Heading>
+                    </Alert>
+                    <Carousel>
+                        <Carousel.Item>
+                            <Image src={startbildschirm} alt="Start Screen" fluid/>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image src={positiv} alt="Positive Outcome" fluid/>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image src={warnung} alt="Warning Message" fluid/>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image src={negativ} alt="Negative Outcome" fluid/>
+                        </Carousel.Item>
+                    </Carousel>
+                    <Card.Text>
+                        Implementierung einer Software für ein Terminal zur Abfrage von Arbeitszeiten und Informationen
+                        zur anstehenden Schicht mit Hilfe von NFC-Technologie
+                    </Card.Text>
+                    <Link to={"https://github.com/labpa/Abfrage-KuKo"}>
+                        <Button style={{width: '350px', backgroundColor:"black", border:"1px solid white", color:"#39868e"}}>GitHub Abfrage Terminal</Button>
+                    </Link>
+                </Card.Body>
+            </Card>
 
-                    <Card className="text-center" style={{backgroundColor: "transparent", color: "white", maxWidth:"600px"}}>
-                        <Card.Body>
-                            <Card.Title>Fahrtenbuch</Card.Title>
-                            <Carousel>
-                                <Carousel.Item>
-                                    <Image src={start} alt="Start Screen" fluid />
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <Image src={angemeldet} alt="Positive Outcome" fluid />
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <Image src={account} alt="Warning Message" fluid />
-                                </Carousel.Item>
-                            </Carousel>
-                            <Card.Text>
-                                With supporting text below as a natural lead-in to additional content.
-                            </Card.Text>
-                            <Button variant="primary" onClick={() => console.log("Button clicked!")}>
-                                Go somewhere
-                            </Button>
-                        </Card.Body>
-                    </Card>
-
-
-
-
-
-
-
+            <Card className="text-center" style={{backgroundColor: "transparent", color: "white", maxWidth: "600px"}}>
+                <Card.Body>
+                    <Alert style={{backgroundColor: "black", color: "white", border: "1px solid white"}}>
+                        <Alert.Heading>Fahrtenbuch</Alert.Heading>
+                    </Alert>
+                    <Carousel>
+                        <Carousel.Item>
+                            <Image src={start} alt="Start Screen" fluid/>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image src={angemeldet} alt="Positive Outcome" fluid/>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image src={account} alt="Warning Message" fluid/>
+                        </Carousel.Item>
+                    </Carousel>
+                    <Card.Text>
+                        Fartenbuch erstellt mit React und Typescript. Übungsprojekt während der Ausbildung.
+                    </Card.Text>
+                    <Link to={"https://github.com/labpa/Fahrtenbuch-KuKo.git"}>
+                        <Button style={{width: '350px', backgroundColor:"black", border:"1px solid white", color:"#39868e"}}>GitHub Fahrtenbuch</Button>
+                    </Link>
+                </Card.Body>
+            </Card>
 
 
         </Container>
