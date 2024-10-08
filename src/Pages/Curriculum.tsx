739c3ef12c232f1e190eafd69e046b3ba540f6af
Pascal <pbienenstein@posteo.de>
2024-10-08 20:50:32 +0200

    wip

--- src/Pages/Curriculum.tsx
+++ src/Pages/Curriculum.tsx
@@ -7,10 +7,10 @@ import Image from "react-bootstrap/Image";
 
 const Corriculum: FC = () => {
     return (
-        <Container className="container-sm p-3">
+        <Container className="container-sm p-3" style={{color:"white"}}>
             <ParticlesComponent id="particles"/>
 
-            <div className="d-flex align-items-center flex-column flex-md-row">
+            <div className="d-flex align-items-center flex-column flex-md-row" >
                 <div>
                     <Image src={foto} rounded style={{width: '250px', height: '300px'}}/>
                 </div>
@@ -22,13 +22,57 @@ const Corriculum: FC = () => {
 
 
             <div className="container-sm content">
+                {/*<Row>*/}
+                {/*    <Col>*/}
+                {/*        <h2 className={"text-justify"}>Fähigkeiten</h2>*/}
+                {/*    </Col>*/}
+                {/*    <Col>*/}
+                {/*        <ul>*/}
+                {/*            <li>*/}
+                {/*                <h3>dfsgfrg</h3>*/}
+                {/*            </li>*/}
+                {/*            <li>*/}
+                {/*                <h3>2016 - 2022 Selbstständiger Monteur - Internationale Umsetzung von Spezialmontagen*/}
+                {/*                    für Film, Theather und Festivals. </h3>*/}
+                {/*            </li>*/}
+                {/*            <li>*/}
+                {/*                <h3>2015 - 2019 Gästebetreuer im Sicherheitsdienst – Internationaler Club, Berlin </h3>*/}
+                {/*            </li>*/}
+                {/*            <li>*/}
+                {/*                <h3>2007 - 2015 Vorarbeiter und Baustellenleiter im Garten- und Landschaftsbau –*/}
+                {/*                    Stuttgart, Wien, Berlin</h3>*/}
+                {/*            </li>*/}
+                {/*        </ul>*/}
+
+
+                {/*    </Col>*/}
+                {/*</Row>*/}
+                {/*<hr style={{borderColor: "#39868E"}} className="border-end border-2 opacity-75"/>*/}
+
+
                 <Row>
                     <Col>
                         <h2 className={"text-justify"}>Berufserfahrung</h2>
                     </Col>
                     <Col>
-                        <h3>Tedfedgferdgferst</h3>
-                        <h3>Teegergregfregst</h3>
+                        <ul>
+                            <li>
+                                <h3>2022 -2024 Fachinformatiker für Anwendungsentwicklung, Leipzig und Lärz</h3>
+                            </li>
+                            <li>
+                                <h3>2016 - 2022 Selbstständiger Monteur - Internationale Umsetzung von Spezialmontagen
+                                    für Film, Theather und Festivals. </h3>
+                            </li>
+                            <li>
+                                <h3>2015 - 2019 Gästebetreuer im Sicherheitsdienst – Internationaler Club, Berlin </h3>
+                            </li>
+                            <li>
+                                <h3>2007 - 2015 Vorarbeiter und Baustellenleiter im Garten- und Landschaftsbau –
+                                    Stuttgart, Wien, Berlin</h3>
+                            </li>
+                        </ul>
+
+
                     </Col>
                 </Row>
                 <hr style={{borderColor: "#39868E"}} className="border-end border-2 opacity-75"/>
@@ -39,13 +83,22 @@ const Corriculum: FC = () => {
                     <Col>
                         <ul>
                             <li>
-                                <h3>09/2022 bis 07/2024: Ausbildung zum Fachinformatiker für Anwendungsentwicklung</h3>
+                                <h3>2024 - Berufsabschluss Fachinformatiker für Anwendungsentwicklung, Leipzig</h3>
                             </li>
                             <li>
-                                <h3>Seilzugangstechniker Fisat Level 1</h3>
+                                <h3>2018 - FISAT Seilzugangs- und Positionierungstechnik Level I, Berlin</h3>
                             </li>
                             <li>
-                                <h3>Garten- und Landschaftsbauer</h3>
+                                <h3>2013 - Weiterbildung Ökologische Gartenpflege, Wien</h3>
+                            </li>
+                            <li>
+                                <h3>2013 - Weiterbildung Rasenpflege, Wien</h3>
+                            </li>
+                            <li>
+                                <h3>2013 - Weiterbildung Bewässerungssysteme, Wien</h3>
+                            </li>
+                            <li>
+                                <h3>2010 - Berufsabschluss Garten- und Landschaftsbauer, Stuttgart</h3>
                             </li>
                         </ul>
                     </Col>
