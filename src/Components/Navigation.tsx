ded36d59cd8a7a6f6c42dc655a55ac174e7c97f7
Pascal <pbienenstein@posteo.de>
2024-10-09 20:30:54 +0200

    wip

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -19,13 +19,13 @@ const Navigation = () => {
                 <Navbar.Toggle aria-controls={"basic-navbar-nav"}/>
                 <Navbar.Collapse id={"basic-navbar-nav"}>
                     <Nav className={"me-auto"}>
-                        <NavLink
-                            className={"nav-link"}
-                            to={"AboutMe"}
-                            style={({ isActive }) => isActive ? activeLinkStyle : linkStyle}
-                        >
-                            Über Mich
-                        </NavLink>
+                        {/*<NavLink*/}
+                        {/*    className={"nav-link"}*/}
+                        {/*    to={"AboutMe"}*/}
+                        {/*    style={({ isActive }) => isActive ? activeLinkStyle : linkStyle}*/}
+                        {/*>*/}
+                        {/*    Über Mich*/}
+                        {/*</NavLink>*/}
                         <NavLink
                             className={"nav-link"}
                             to={"Curriculum"}
