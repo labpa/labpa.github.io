222a4e2e180ddd1bc18bb787cbeb0e59a981ecff
Pascal <pbienenstein@posteo.de>
2024-10-12 00:42:14 +0200

    wip -> anmeldeung etc

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -15,28 +15,55 @@ const Navigation = () => {
         textDecoration: "none",
     };
 
-    return(
-        <Navbar expand={"lg"} style={{backgroundColor: "#39868e"}}>
-            <Container>
-                <Navbar.Brand href={"/"} style={{color: "#edecd8"}}>Pascal Bienenstein</Navbar.Brand>
-                <Navbar.Toggle aria-controls={"basic-navbar-nav"}/>
-                <Navbar.Collapse id={"basic-navbar-nav"}>
-                    <Nav className={"me-auto"}>
-                        {/*<NavLink*/}
-                        {/*    className={"nav-link"}*/}
-                        {/*    to={"AboutMe"}*/}
-                        {/*    style={({ isActive }) => isActive ? activeLinkStyle : linkStyle}*/}
-                        {/*>*/}
-                        {/*    Über Mich*/}
-                        {/*</NavLink>*/}
-                        <NavLink className={"nav-link"} to={"Curriculum"} style={({ isActive }) => isActive ? activeLinkStyle : linkStyle}>Lebenslauf</NavLink>
-                        <NavLink className={"nav-link"} to={"Projects"} style={({ isActive }) => isActive ? activeLinkStyle : linkStyle}>Projekte</NavLink>
-                        <NavLink className={"nav-link"} to={"Signin"} style={({ isActive }) => isActive ? activeLinkStyle : linkStyle}>Login</NavLink>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
+    if(!userinfo){
+        return(
+            <Navbar expand={"lg"} style={{backgroundColor: "#39868e"}}>
+                <Container>
+                    <Navbar.Brand href={"/"} style={{color: "#edecd8"}}>Pascal Bienenstein</Navbar.Brand>
+                    <Navbar.Toggle aria-controls={"basic-navbar-nav"}/>
+                    <Navbar.Collapse id={"basic-navbar-nav"}>
+                        <Nav className={"me-auto"}>
+                            {/*<NavLink*/}
+                            {/*    className={"nav-link"}*/}
+                            {/*    to={"AboutMe"}*/}
+                            {/*    style={({ isActive }) => isActive ? activeLinkStyle : linkStyle}*/}
+                            {/*>*/}
+                            {/*    Über Mich*/}
+                            {/*</NavLink>*/}
+                            <NavLink className={"nav-link"} to={"Curriculum"} style={({ isActive }) => isActive ? activeLinkStyle : linkStyle}>Lebenslauf</NavLink>
+                            <NavLink className={"nav-link"} to={"Projects"} style={({ isActive }) => isActive ? activeLinkStyle : linkStyle}>Projekte</NavLink>
+                            <NavLink className={"nav-link"} to={"Signin"} style={({ isActive }) => isActive ? activeLinkStyle : linkStyle}>Login</NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        )
+    } else{
+        return(
+            <Navbar expand={"lg"} style={{backgroundColor: "#39868e"}}>
+                <Container>
+                    <Navbar.Brand href={"/"} style={{color: "#edecd8"}}>Pascal Bienenstein</Navbar.Brand>
+                    <Navbar.Toggle aria-controls={"basic-navbar-nav"}/>
+                    <Navbar.Collapse id={"basic-navbar-nav"}>
+                        <Nav className={"me-auto"}>
+                            {/*<NavLink*/}
+                            {/*    className={"nav-link"}*/}
+                            {/*    to={"AboutMe"}*/}
+                            {/*    style={({ isActive }) => isActive ? activeLinkStyle : linkStyle}*/}
+                            {/*>*/}
+                            {/*    Über Mich*/}
+                            {/*</NavLink>*/}
+                            <NavLink className={"nav-link"} to={"Curriculum"} style={({ isActive }) => isActive ? activeLinkStyle : linkStyle}>Lebenslauf</NavLink>
+                            <NavLink className={"nav-link"} to={"Projects"} style={({ isActive }) => isActive ? activeLinkStyle : linkStyle}>Projekte</NavLink>
+                            <NavLink className={"nav-link"} to={"Angemeldet"} style={({ isActive }) => isActive ? activeLinkStyle : linkStyle}>Angemeldet</NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        )
+    }
+
+
 }
 
 export default Navigation;
--- src/Pages/Angemeldet.tsx
+++ src/Pages/Angemeldet.tsx
@@ -1,10 +1,14 @@
 import React, {FC} from "react";
+import {Container} from "react-bootstrap";
+import ParticlesComponent from "../Components/particles";
 
 const Angemeldet : FC = () => {
     return(
-        <div>
-            <h1>Angemeldet</h1>
-        </div>
+        <Container
+            className="container-sm d-flex flex-column justify-content-center align-items-center text-center p-3">
+            <ParticlesComponent id="particles"/>
+            <h1 className="mb-3 text-white">Angemeldet</h1>
+        </Container>
     )
 }
 
